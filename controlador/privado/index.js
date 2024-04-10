583658082f060a262000bbe5370adf6ddd969999
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-04-09 22:58:57 -0600

    Index terminado- solo faltan modals

--- controlador/privado/index.js
+++ controlador/privado/index.js
@@ -1,5 +1,39 @@
+const LOGIN_FORM = document.getElementById('LoginForm');
+const CONREC_FORM = document.getElementById('RecForm');
+const CONREST_FORM = document.getElementById('RestForm');
+
+const textREC = document.getElementById('textREC');
+const textREST = document.getElementById('textREST');
+
 // *Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
-    // *Llamada a la función para mostrar el encabezado y pie del documento.
-    loadTemplate();
+    // Se muestra el formulario para iniciar sesión.
+    LOGIN_FORM.classList.remove('d-none');
+    // Se oculta el formulario de restablecimiento de contraseña (paso 1 y 2).
+    CONREC_FORM.classList.add('d-none');
+    CONREST_FORM.classList.add('d-none');
 });
+
+function showRecCon() {
+    LOGIN_FORM.classList.add('d-none');
+    CONREC_FORM.classList.remove('d-none');
+    textREC.classList.remove('d-none');
+    textREST.classList.add('d-none');
+    CONREST_FORM.classList.add('d-none');
+}
+
+function showRestCon() {
+    LOGIN_FORM.classList.add('d-none');
+    CONREC_FORM.classList.add('d-none');
+    textREC.classList.add('d-none');
+    textREST.classList.remove('d-none');
+    CONREST_FORM.classList.remove('d-none');
+}
+
+function showLogin() {
+    LOGIN_FORM.classList.remove('d-none');
+    CONREC_FORM.classList.add('d-none');
+    textREC.classList.add('d-none');
+    textREST.classList.add('d-none');
+    CONREST_FORM.classList.add('d-none');
+}
