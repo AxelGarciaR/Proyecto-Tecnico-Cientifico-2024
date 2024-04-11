ba4e0918165580f233d89165c942923df19db921
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-04-10 22:53:05 -0600

    Automoviles - filtros - terminado un 90% faltan modelos especificos

--- controlador/privado/automoviles.js
+++ controlador/privado/automoviles.js
@@ -2,3 +2,26 @@
 document.addEventListener('DOMContentLoaded', async () => {
     loadTemplate();
 });
+
+function rotarImagen(idImagen) {
+    var imagen = document.getElementById(idImagen);
+    if (!imagen.classList.contains('rotacion-90')) {
+        imagen.classList.add('rotacion-90');
+    } else {
+        imagen.classList.remove('rotacion-90');
+    }
+}
+
+$('#datepicker-desde').datepicker({
+    uiLibrary: 'bootstrap5'
+});
+$('#datepicker-hasta').datepicker({
+    uiLibrary: 'bootstrap5'
+});
+
+$('#datepicker-desdeRE').datepicker({
+    uiLibrary: 'bootstrap5'
+});
+$('#datepicker-hastaRE').datepicker({
+    uiLibrary: 'bootstrap5'
+});
