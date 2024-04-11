53f83474aecb418bbfa520e8b3b71a4548b0b380
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-04-11 00:09:21 -0600

    Automoviles - filtros - 100% terminado

--- controlador/privado/automoviles.js
+++ controlador/privado/automoviles.js
@@ -1,3 +1,9 @@
+const MODELOS_CONTAINER = document.getElementById('contenedor-modelos');
+const MARCAS_CONTAINER = document.getElementById('contenedor-marcas');
+
+const BUSCAR_MODELOS = document.getElementById('btnBuscarModelos');
+const REGRESAR_MARCAS = document.getElementById('btnReturn');
+
 // *Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     loadTemplate();
@@ -10,6 +16,22 @@ function rotarImagen(idImagen) {
     } else {
         imagen.classList.remove('rotacion-90');
     }
+}
+
+function showModelos() {
+    MODELOS_CONTAINER.classList.remove('d-none');
+    MARCAS_CONTAINER.classList.add('d-none');
+
+    BUSCAR_MODELOS.classList.add('d-none');
+    REGRESAR_MARCAS.classList.remove('d-none');
+}
+
+function showMarcas() {
+    MODELOS_CONTAINER.classList.add('d-none');
+    MARCAS_CONTAINER.classList.remove('d-none');
+
+    BUSCAR_MODELOS.classList.remove('d-none');
+    REGRESAR_MARCAS.classList.add('d-none');
 }
 
 $('#datepicker-desde').datepicker({
