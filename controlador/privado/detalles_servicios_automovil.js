46e82142aff9ffaab71ae75e18313ab1406407f0
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-04-17 18:55:09 -0600

    Historial de servicios realizados a un vehiculo - hecho - solo falta responsive

--- controlador/privado/detalles_servicios_automovil.js
+++ controlador/privado/detalles_servicios_automovil.js
@@ -1,3 +1,6 @@
+const CARDS_SERVICIOS = document.getElementById('container-cards');
+const DETAILS = document.getElementById('contenedor-details');
+
 // *Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     loadTemplate();
@@ -5,4 +8,13 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 function goBack() {
     window.history.back();
+}
+function showDetails() {
+    CARDS_SERVICIOS.classList.add('d-none');
+    DETAILS.classList.remove('d-none');
+}
+
+function Return() {
+    CARDS_SERVICIOS.classList.remove('d-none');
+    DETAILS.classList.add('d-none');
 }
