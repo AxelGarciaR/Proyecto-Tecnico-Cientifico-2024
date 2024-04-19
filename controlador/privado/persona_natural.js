05663410580d8fb50fbf722a310f8c92abbf16c5
emurill0 <20220021@ricaldone.edu.sv>
2024-04-19 10:08:35 -0600

    nuevos cambios

---
+++ controlador/privado/persona_natural.js
@@ -0,0 +1,25 @@
+// *Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', async () => {
+    loadTemplate();
+});
+
+
+
+document.getElementById('input_dui').addEventListener('input', function (event) {
+    // Obtener el valor actual del campo de texto
+    let inputValue = event.target.value;
+
+    // Limpiar el valor de cualquier carácter que no sea un número
+    inputValue = inputValue.replace(/\D/g, '');
+
+    // Asegurar que no haya más de 9 dígitos
+    inputValue = inputValue.slice(0, 9);
+
+    // Formatear el número agregando el guión antes del último dígito si hay al menos dos dígitos
+    if (inputValue.length > 1) {
+        inputValue = inputValue.slice(0, -1) + '-' + inputValue.slice(-1);
+    }
+
+    // Actualizar el valor del campo de texto con la entrada formateada
+    event.target.value = inputValue;
+});
