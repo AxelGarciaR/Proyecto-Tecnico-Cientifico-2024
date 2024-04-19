9584ad9247052c80e3334be7140abc6e022a1277
adianitafresita <20190015@ricaldone.edu.sv>
2024-04-19 11:21:01 -0600

    comentado clientes js

--- controlador/privado/cliente.js
+++ controlador/privado/cliente.js
@@ -1,10 +1,8 @@
-
-
 // *Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     loadTemplate();
 });
-
-function gotoDetailsCar(){
+// *Funcion para ir a los detalles del automovil
+function gotoDetailsCar() {
     location.href = "../../vistas/privado/detalles_automovil.html";
 }
