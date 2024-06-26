26d4ef47b7f022ef594ee0779f7b51a634385480
Axel <20220127@ricaldone.edu.sv>
2024-06-26 09:29:16 -0600

    Base de datos actualizada

--- darg_database.sql
+++ darg_database.sql
@@ -3,31 +3,44 @@ DROP DATABASE IF EXISTS darg_database;
 CREATE DATABASE darg_database;
 USE darg_database;
 
-CREATE TABLE tb_departamentos
-(
-	id_departamento INT AUTO_INCREMENT PRIMARY KEY,
-	nombre_departamento VARCHAR(100) NOT NULL
-);
-
 CREATE TABLE tb_clientes
 (
 	id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-	dui_cliente VARCHAR(10) NOT NULL UNIQUE,
-	telefono_cliente VARCHAR(9) NOT NULL UNIQUE,
-	correo_cliente VARCHAR(50) NOT NULL UNIQUE,
+    fecha_registro_cliente DATE,
+	dui_cliente VARCHAR(10) NOT NULL,
+	telefono_cliente VARCHAR(9) NOT NULL,
+	correo_cliente VARCHAR(50) NOT NULL,
 	nombres_cliente VARCHAR(50) NOT NULL ,
 	apellidos_cliente VARCHAR(50) NOT NULL,
 	tipo_cliente ENUM('Persona natural','Persona juridica') NOT NULL,
-	id_departamento INT ,
+	departamento_cliente ENUM('Ahuachapán', 'Cabañas', 'Chalatenango', 'Cuscatlán', 'La Libertad', 'La Paz', 'La Unión', 'Morazán', 'San Miguel', 'San Salvador', 'San Vicente', 'Santa Ana', 'Sonsonate', 'Usulután'),
 	NIT_cliente VARCHAR(18) NULL,
 	NRC_cliente VARCHAR(11) NULL,
     NRF_cliente VARCHAR(25) NULL,
-    giro_cliente VARCHAR(25) NULL
+    rubro_comercial ENUM('Alimenticio','Automotriz', 'Belleza', 'Calzado') NULL,
+    estado_cliente ENUM('Activo', 'Eliminado'),
+    fto_cliente VARCHAR(50)
 );
+
+SELECT * FROM tb_clientes;
+
+SELECT * FROM tb_clientes 
+WHERE tipo_cliente = 'Persona Natural' 
+AND (
+    CONCAT(nombres_cliente, " ", apellidos_cliente) LIKE '%Adriana%' OR 
+    dui_cliente LIKE '%Adriana%' OR 
+    telefono_cliente LIKE '%Adriana%' OR 
+    correo_cliente LIKE'%Adriana%' OR 
+    NIT_cliente LIKE '%Adriana%' OR 
+    NRC_cliente LIKE '%Adriana%' OR
+    NRF_cliente LIKE '%Adriana%'
+) 
+AND departamento_cliente = 'Cabañas' 
+AND fecha_registro_cliente >= '2024-07-11'
+AND rubro_comercial = '' ;
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_clientes
-  ADD CONSTRAINT u_fk_departamento_cliente FOREIGN KEY(id_departamento) references tb_departamentos(id_departamento),
   ADD CONSTRAINT u_dui_cliente UNIQUE (dui_cliente),
   ADD CONSTRAINT u_telefono_cliente UNIQUE (telefono_cliente),
   ADD CONSTRAINT u_correo_cliente UNIQUE (correo_cliente);
@@ -48,7 +61,7 @@ CREATE TABLE tb_modelos_automoviles
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_modelos_automoviles
-  ADD CONSTRAINT u_fk_marca_automovil_modelo_automovil FOREIGN KEY (id_marca_automovil) REFERENCES tb_marcas_automoviles(id_marca_automovil);
+ADD CONSTRAINT u_fk_marca_automovil_modelo_automovil FOREIGN KEY (id_marca_automovil) REFERENCES tb_marcas_automoviles(id_marca_automovil);
 
 CREATE TABLE tb_tipos_automoviles
 (
@@ -72,7 +85,8 @@ CREATE TABLE tb_automoviles
 	placa_automovil VARCHAR(8),
 	imagen_automovil VARCHAR(25) NOT NULL,
 	id_cliente INT, /*FK*/
-	fecha_registro DATE NOT NULL
+	fecha_registro DATE NOT NULL,
+    estado_automovil ENUM('Activo', 'Eliminado') NOT NULL
 );
 
 /**FOREIGN KEY**/
@@ -102,13 +116,14 @@ CREATE TABLE tb_servicios
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_servicios
-  ADD CONSTRAINT u_fk_tipo_servicio_servicio FOREIGN KEY (id_tipo_servicio) REFERENCES tb_tipos_servicios(id_tipo_servicio);
+ADD CONSTRAINT u_fk_tipo_servicio_servicio FOREIGN KEY (id_tipo_servicio) REFERENCES tb_tipos_servicios(id_tipo_servicio);
 
 CREATE TABLE tb_formas_pagos
 (
 	id_forma_pago INT PRIMARY KEY AUTO_INCREMENT,
 	nombre_forma_pago VARCHAR(100)
 );
+
 CREATE TABLE tb_citas
 (
 	id_cita INT PRIMARY KEY AUTO_INCREMENT,
@@ -119,12 +134,12 @@ CREATE TABLE tb_citas
     zona_habilitada ENUM('Ayutuxtepeque',  'Aguilares') NULL,  
 	direccion_ida VARCHAR(250) NULL,  
 	direccion_regreso VARCHAR(250) NULL,
-    estado_cita ENUM('En espera', 'Aceptado', 'Cancelado') /**(Automáticamente se borre en 24 horas )**/ NOT NULL
+    estado_cita ENUM('En espera', 'Aceptado', 'Cancelado', 'Finalizada') /**(Automáticamente se borre en 24 horas )**/ NOT NULL
 );
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_citas
-  ADD CONSTRAINT u_fk_automovil_cita FOREIGN KEY (id_automovil) REFERENCES tb_automoviles(id_automovil);
+ADD CONSTRAINT u_fk_automovil_cita FOREIGN KEY (id_automovil) REFERENCES tb_automoviles(id_automovil);
 
 CREATE TABLE tb_servicios_en_proceso
 (
@@ -138,8 +153,8 @@ CREATE TABLE tb_servicios_en_proceso
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_servicios_en_proceso
-  ADD CONSTRAINT u_fk_cita_servicio_en_proceso FOREIGN KEY (id_cita) REFERENCES tb_citas(id_cita),
-  ADD CONSTRAINT u_fk_servicio_servicio_en_proceso FOREIGN KEY (id_servicio) REFERENCES tb_servicios(id_servicio);
+ADD CONSTRAINT u_fk_cita_servicio_en_proceso FOREIGN KEY (id_cita) REFERENCES tb_citas(id_cita),
+ADD CONSTRAINT u_fk_servicio_servicio_en_proceso FOREIGN KEY (id_servicio) REFERENCES tb_servicios(id_servicio);
 
 CREATE TABLE tb_consumidores_finales
 (
@@ -168,8 +183,8 @@ CREATE TABLE tb_detalles_consumidores_finales
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_detalles_consumidores_finales
-  ADD CONSTRAINT u_fk_consumidor_final_detalle_consumidor_final FOREIGN KEY (id_consumidor_final) REFERENCES tb_consumidores_finales(id_consumidor_final),
-  ADD CONSTRAINT u_fk_servicio_detalle_consumidor_final FOREIGN KEY (id_servicio) REFERENCES tb_servicios(id_servicio);
+ADD CONSTRAINT u_fk_consumidor_final_detalle_consumidor_final FOREIGN KEY (id_consumidor_final) REFERENCES tb_consumidores_finales(id_consumidor_final),
+ADD CONSTRAINT u_fk_servicio_detalle_consumidor_final FOREIGN KEY (id_servicio) REFERENCES tb_servicios(id_servicio);
 
 CREATE TABLE tb_creditos_fiscales
 (
@@ -181,14 +196,14 @@ CREATE TABLE tb_creditos_fiscales
     nota VARCHAR(100),
 	id_cita  INT, /**FK**/
     nombre_emisor VARCHAR(25) NULL,
-    dui_emisor VARCHAR(10) NOT NULL UNIQUE,
+    dui_emisor VARCHAR(10) NOT NULL,
     estado_credito_fiscal ENUM('En espera', 'Cancelado', 'Completado') NOT NULL
 );
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_creditos_fiscales
-  ADD CONSTRAINT u_fk_cita_credito_fiscal FOREIGN KEY (id_cita) REFERENCES tb_citas(id_cita),
-  ADD CONSTRAINT u_dui_emisor UNIQUE (dui_emisor);
+ADD CONSTRAINT u_fk_cita_credito_fiscal FOREIGN KEY (id_cita) REFERENCES tb_citas(id_cita),
+ADD CONSTRAINT u_dui_emisor UNIQUE (dui_emisor);
 /**Constraint**/
 
 CREATE TABLE tb_detalles_creditos_fiscales 
@@ -202,8 +217,8 @@ CREATE TABLE tb_detalles_creditos_fiscales
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_detalles_creditos_fiscales
-  ADD CONSTRAINT u_fk_credito_fiscal_detalle_credito_fiscal FOREIGN KEY (id_credito_fiscal) REFERENCES tb_creditos_fiscales(id_credito_fiscal),
-  ADD CONSTRAINT u_fk_servicio_detalle_credito_fiscal FOREIGN KEY (id_servicio) REFERENCES tb_servicios(id_servicio);
+ADD CONSTRAINT u_fk_credito_fiscal_detalle_credito_fiscal FOREIGN KEY (id_credito_fiscal) REFERENCES tb_creditos_fiscales(id_credito_fiscal),
+ADD CONSTRAINT u_fk_servicio_detalle_credito_fiscal FOREIGN KEY (id_servicio) REFERENCES tb_servicios(id_servicio);
   
 CREATE TABLE tb_seguimientos_consumidores_finales
 (
@@ -215,7 +230,7 @@ CREATE TABLE tb_seguimientos_consumidores_finales
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_seguimientos_consumidores_finales
-  ADD CONSTRAINT u_fk_consumidor_final_seguimiento_consumidor_final FOREIGN KEY (id_consumidor_final) REFERENCES tb_consumidores_finales(id_consumidor_final);
+ADD CONSTRAINT u_fk_consumidor_final_seguimiento_consumidor_final FOREIGN KEY (id_consumidor_final) REFERENCES tb_consumidores_finales(id_consumidor_final);
 
 CREATE TABLE tb_seguimientos_creditos_fiscales
 (
@@ -225,6 +240,9 @@ CREATE TABLE tb_seguimientos_creditos_fiscales
 	id_consumidor_final INT /* FK */
 );
 
+/**FOREIGN KEY**/
+ALTER TABLE tb_seguimientos_creditos_fiscales
+ADD CONSTRAINT u_fk_seguimiento_credito_fiscal_consumidor_final FOREIGN KEY (id_consumidor_final) REFERENCES tb_consumidores_finales(id_consumidor_final);
 
 CREATE TABLE tb_usuarios_clientes
 (
@@ -236,7 +254,7 @@ CREATE TABLE tb_usuarios_clientes
 
 /**FOREIGN KEY**/
 ALTER TABLE tb_usuarios_clientes
-  ADD CONSTRAINT u_fk_cliente_usuarios_clientes FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id_cliente);
+ADD CONSTRAINT u_fk_cliente_usuarios_clientes FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id_cliente);
 
 
 CREATE TABLE tb_especializaciones_trabajadores
@@ -250,33 +268,34 @@ CREATE TABLE tb_trabajadores
 (
 	id_trabajador INT PRIMARY KEY AUTO_INCREMENT, #PK
 	id_especializacion_trabajador INT NOT NULL, /*FK*/
-	dui_trabajador VARCHAR(9) NOT NULL UNIQUE,  #NN U 
-	telefono_trabajador VARCHAR(9) NOT NULL UNIQUE, #NN U
-	correo_trabajador VARCHAR(50) NOT NULL UNIQUE, #NN U 
+	dui_trabajador VARCHAR(9) NOT NULL,  #NN U 
+	telefono_trabajador VARCHAR(9) NOT NULL, #NN U
+	correo_trabajador VARCHAR(50) NOT NULL, #NN U 
 	nombres_trabajador VARCHAR(50) NOT NULL, #NN
 	apellidos_trabajador VARCHAR(50) NOT NULL, #NN
-	id_departamento INT, /*FK*/
+	departamento_trabajador ENUM('Ahuachapán', 'Cabañas', 'Chalatenango', 'Cuscatlán', 'La Libertad', 'La Paz', 'La Unión', 'Morazán', 'San Miguel', 'San Salvador', 'San Vicente', 'Santa Ana', 'Sonsonate', 'Usulután'),  
 	NIT_trabajador VARCHAR(18) NULL, #N U
 	fecha_contratacion DATE NOT NULL, 
 	salario_base DECIMAL(5, 2) NOT NULL,
     Fto_trabajador LONGBLOB
 );
 
+SELECT * FROM tB_trabajadores;
+
 /**FOREIGN KEY**/
 ALTER TABLE tb_trabajadores
-  ADD CONSTRAINT u_fk_especializacion_trabajador_trabajador FOREIGN KEY (id_especializacion_trabajador) REFERENCES tb_especializaciones_trabajadores(id_especializacion_trabajador),
-  ADD CONSTRAINT u_fk_departamento_trabajador FOREIGN KEY (id_departamento) REFERENCES tb_departamentos(id_departamento),
-  ADD CONSTRAINT u_dui_trabajador UNIQUE (dui_trabajador),
-  ADD CONSTRAINT u_telefono_trabajador UNIQUE (telefono_trabajador),
-  ADD CONSTRAINT u_correo_trabajador UNIQUE (correo_trabajador),
-  ADD CONSTRAINT u_NIT_trabajador UNIQUE (NIT_trabajador);
+ADD CONSTRAINT u_fk_especializacion_trabajador_trabajador FOREIGN KEY (id_especializacion_trabajador) REFERENCES tb_especializaciones_trabajadores(id_especializacion_trabajador),
+ADD CONSTRAINT u_dui_trabajador UNIQUE (dui_trabajador),
+ADD CONSTRAINT u_telefono_trabajador UNIQUE (telefono_trabajador),
+ADD CONSTRAINT u_correo_trabajador UNIQUE (correo_trabajador),
+ADD CONSTRAINT u_NIT_trabajador UNIQUE (NIT_trabajador);
     /**Constraint**/
 
 CREATE TABLE tb_usuarios
 (
 	id_usuario INT PRIMARY KEY AUTO_INCREMENT, #PK
-    correo_usuario VARCHAR(500),
-	clave_usuario VARCHAR(60) /*(min-6 max-60)*/,
+    correo_usuario VARCHAR(200),
+	clave_usuario VARCHAR(100) /*(min-6 max-50)*/,
     telefono_usuario VARCHAR(9),
     tipo_usuario ENUM('Administrador') NOT NULL
 );
@@ -289,5 +308,5 @@ CREATE TABLE tb_formas_pagos_consumidores_finales
 );
 
 ALTER TABLE tb_formas_pagos_consumidores_finales
-  ADD CONSTRAINT u_fk_forma_pago_forma_pago_consumidor_final FOREIGN KEY (id_forma_pago) REFERENCES tb_formas_pagos(id_forma_pago),
-  ADD CONSTRAINT u_fk_consumidor_final_forma_pago_forma_pago_consumidor_final FOREIGN KEY (id_consumidor_final) REFERENCES tb_consumidores_finales(id_consumidor_final);
+ADD CONSTRAINT u_fk_forma_pago_forma_pago_consumidor_final FOREIGN KEY (id_forma_pago) REFERENCES tb_formas_pagos(id_forma_pago),
+ADD CONSTRAINT u_fk_consumidor_final_forma_pago_forma_pago_consumidor_final FOREIGN KEY (id_consumidor_final) REFERENCES tb_consumidores_finales(id_consumidor_final);
